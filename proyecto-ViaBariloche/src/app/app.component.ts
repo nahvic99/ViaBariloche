e48e54f3a845c46cc641cb2120670a9680e2d8be
NahVicDev <39022314+nahvic99@users.noreply.github.com>
2020-10-20 23:31:05 -0300

    Administrador de Articulos

---
+++ proyecto-ViaBariloche/src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticulosService } from './articulos.service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent implements OnInit{
+  title = 'proyecto-ViaBariloche';
+  articulos = null;
+  art = {
+    codigo: null,
+    descripcion: null,
+    precio: null,
+  }
+
+  constructor(private articulosServicio: ArticulosService) {}
+
+  ngOnInit() {
+    this.recuperarTodos();
+  }
+
+  recuperarTodos() {
+    this.articulosServicio.recuperarTodos().subscribe(result => this.articulos = result);
+  }
+
+  alta() {
+    this.articulosServicio.alta(this.art).subscribe(datos => {
+      if (datos['resultado']=='OK') {
+        alert(datos['mensaje']);
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  baja(codigo) {
+    this.articulosServicio.baja(codigo).subscribe(datos => {
+      if (datos['resultado']=='OK') {
+        alert(datos['mensaje']);
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  modificacion() {
+    this.articulosServicio.modificacion(this.art).subscribe(datos => {
+      if (datos['resultado']=='OK') {
+        alert(datos['mensaje']);
+        this.recuperarTodos();
+      }
+    });    
+  }
+
+  seleccionar(codigo) {
+    this.articulosServicio.seleccionar(codigo).subscribe(result => this.art = result[0]);
+  }
+
+  hayRegistros() {
+    return true;
+  } 
+
+}
